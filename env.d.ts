cc1cdc2e4ddbe8306c16a4bcde9fc538f75bbd52
DominikDO98 <dominiklas13@wp.pl>
2025-07-29 18:50:57 +0200

    Feat: Created Dockerfile and prepared for depolymenyt
    Feat: Created Dockerfile
    
    Feat: Created build script
    
    Feat: Added dist diractory to .gitignore
    
    Feat: Created working builder stage and main stage  in Dockerfile
    
    Fix: Commented COPY .env .env line in Dockerfile
    
    Fix: changed start script to use node instead of tsx
    
    Fix: Added line in Dockerfile for installing lib dependacies
    
    Fix: Changed type of imports to ESModules
    
    Feat: Added git submodule update command to pull lib submodule to container wheile deploing the app
    
    Fix: Added ls command to see what's going on
    
    Feat: Copied .git file and corrceted spelling mistake in git submodule update command
    
    Feat: Another small fix
    
    Fix: Added git to builder stage in Dockerfile
    
    Refactor: Turn lib submodule into regular directory
    
    Feat: Added logs diractory to .gitignore
    
    Feat: Update Dockerfile
    
    Feat: Updated imports and tsconfig.json
    
    Feat: added type for type import in score.controller
    
    Feat: Changed submodule lib into regular directory
    
    Fix: Changed all imports to esmodule
    
    Fix: changed __dirname to process.cwd() in logger, updated version of amqplib and fixed subsequent import errors
    
    Fix: Fixed start script for build version, removed unnecessary console.log
    
    Fix: added coonsole log to check uri
    
    Refactor: Remove console.logs
    
    Feat: Updated broker
    
    Feat: Updated env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -2,21 +2,14 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      githubClientID: string;
-      githubClientSecret: string;
-      authorizeURL: string;
-      tokenURL: string;
-      apiURLBase: string;
-      baseURL: string;
-      frontendURL: string;
-
       MONGODB_DB: string;
       MONGODB_URI: string;
 
       RABBITMQ_DEFAULT_PASS: string;
       RABBITMQ_DEFAULT_USER: string;
-      RABBITMQ_DEFAULT_VHOST: string;
-      RABBITMQ_URI: string;
+      RABBITMQ_PRIVATE_URL: string;
+
+      TIMEOUT: string;
     }
   }
 }
